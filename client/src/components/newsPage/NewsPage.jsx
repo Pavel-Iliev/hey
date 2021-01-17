30b629b7a5f187da9b1a0b75298d005470bbb6d5
Pavel-Iliev <p88iliev@gmail.com>
2021-01-17 15:11:29 +0100

    add animation to the news card

--- client/src/components/newsPage/NewsPage.jsx
+++ client/src/components/newsPage/NewsPage.jsx
@@ -1,9 +1,12 @@
 import './style-newsPage.css';
-import { SwipeableList, SwipeableListItem, ActionAnimations } from '@sandstreamdev/react-swipeable-list';
+import { SwipeableList, SwipeableListItem } from '@sandstreamdev/react-swipeable-list';
 import '@sandstreamdev/react-swipeable-list/dist/styles.css';
+import { InView } from 'react-intersection-observer';
 
 function NewsPage(props) {
   const { newsForPage, addNewsToPersonal, deleteOneNews } = props;
+
+  console.log(newsForPage)
 
   //save new to personals news
   function save(author, description, publishedAt, source, title, url, urlToImage) {
@@ -13,9 +16,25 @@ function NewsPage(props) {
   //test to delete method
   function deleteSelectedNews(id) {
     deleteOneNews(id);
+  }  
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
+  }
+  
+  function addNewsAnimation(card) {
+    card.style.transform = `translate(${getRandomInt(-3, 3)}%, ${getRandomInt(-4, 4)}%) rotate(${getRandomInt(-4, 4)}deg)`;  
+    card.style.WebkitTransition = 'transform 1s';
+    card.style.MozTransition = 'transform 1s';
   }
 
-  console.log(newsForPage)
+  function removeNewsAnimation(card) {
+    card.style.transform = `translate(0,0) rotate(0}deg)`;  
+    card.style.WebkitTransition = 'transform 0s';
+    card.style.MozTransition = 'transform 0s';
+  }
 
   return(
     <>
@@ -24,22 +43,37 @@ function NewsPage(props) {
           newsForPage ? 
         <SwipeableList>
           {newsForPage.map(e => {
-            return <SwipeableListItem
-              swipeLeft={{
-                content: <div>figa la troia</div>,
-                action: () => deleteSelectedNews(e._id)
-              }}
-              swipeRight={{
-                content: <div>FEEEEEGA</div>,
-                action: () => {
-                  save(e.author, e.description, e.publishedAt, e.source, e.title, e.url, e.urlToImage)
-                }
-              }}
-              //onSwipeProgress={progress => console.info(`Swipe progress: ${progress}%`)}
-              key={e._id}
-            >
-              <p>{e.author}</p>
-            </SwipeableListItem>
+            return <InView 
+                      className="news-card"
+                      onChange={(inView, entry) => {
+                      if(inView) {
+                        console.log(entry)
+                        addNewsAnimation(entry.target)
+                        
+                      } else {
+                        removeNewsAnimation(entry.target)
+                      }
+                      }}
+                      key={e._id}
+                    >
+                      <SwipeableListItem
+                        swipeLeft={{
+                          content: <div>figa la troia</div>,
+                          action: () => deleteSelectedNews(e._id)
+                        }}
+                        swipeRight={{
+                          content: <div>FEEEEEGA</div>,
+                          action: () => {
+                            save(e.author, e.description, e.publishedAt, e.source, e.title, e.url, e.urlToImage)
+                          }
+                        }}
+                        //onSwipeProgress={progress => console.info(`Swipe progress: ${progress}%`)}
+                      >
+                        
+                          <p>{e.author}</p> 
+                          <h2>Plain children are always rendered. Use onChange to monitor state.</h2>
+                      </SwipeableListItem>
+                    </InView>
           })}
         </SwipeableList>
         : 'undefined'
